dab725983ac30173a50e272f387a9d2c241de0c6
pedrin453 <pedro.francisco@iesb.edu.br>
2025-03-11 23:16:51 -0300

    Alterações

--- 03 - Revisão javascript/exercicio/exercicio 01/atividade07.js
+++
@@ -1,8 +0,0 @@
-/*
-Exercício 7: Métodos de Arrays
-Crie um script que receba um array de strings e faça as seguintes operações:
-1. Converta todas as strings para maiúsculas.
-2. Filtre as strings que começam com a letra 'A'.
-3. Crie um novo array com o comprimento de cada string.
-Utilize o array ["Maçã", "Banana", "Abacaxi", "Laranja"] para os testes.
-*/
