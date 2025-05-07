6613584427ee87a74c1e6f05e8a3061702bafc66
pedrin453 <pedro.francisco@iesb.edu.br>
2025-05-06 21:18:31 -0300

    RECEITA-APP

---
+++ atividades 02/receitas-app/App.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: { backgroundColor: '#6200ee' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold' },
+          }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Receitas' }}
+          />
+          <Stack.Screen
+            name="Receita"
+            component={ReceitaScreen}
+            options={({ route }) => ({ title: route.params.nome })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
